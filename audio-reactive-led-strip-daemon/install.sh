dc3d599e52a780feefd823b5c7f9319aa24f70dc
Alex Treil <treilalex@gmail.com>
2020-03-20 05:17:20 +0000

    Add service file for audio reactive led strip.

---
+++ audio-reactive-led-strip-daemon/install.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+SVC=audio-reactive-led-strip.service
+echo "installing ${SVC}..."
+
+systemctl is-enabled ${SVC}
+
+#if [ $? -eq 0 ]
+#then
+#    echo "${SVC} already installed and enabled"
+#    exit 0
+#fi
+
+SRC="${PWD}/${SVC}"
+DEST="/lib/systemd/system/"
+
+echo "copying ${SRC} to ${DEST}"
+cp ${SRC} ${DEST}
+
+if [ $? -ne 0 ]
+then
+    echo "mv failed"
+    exit 1
+fi
+
+systemctl start ${SVC}
+if [ $? -ne 0 ]
+then
+    echo "failed to start ${SVC}"
+    exit 1
+fi
+
+systemctl enable ${SVC}
+if [ $? -ne 0 ]
+then
+    echo "failed to enable ${SVC} at startup"
+    exit 1
+fi
+
+echo "successfully installed ${SVC}"
+
+systemctl daemon-reload
+if [ $? -ne 0 ]
+then
+	echo "failed to reload daemon"
+	exit 1
+fi
+
+exit 0
